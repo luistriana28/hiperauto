2c04fc096a36b244734a979c10179048ab557314
Alan Ramos <alan196@gmail.com>
2014-02-04 17:26:07 -0600

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+ 
+{
+    "name": "Taller Mecánico",
+    "version": "1.0",
+    "description": 
+    """
+        Agrega campos al pedido de ventas, adaptado a un taller mecánico
+    """,
+    "author": "MAR Sistemas",
+    "website": "http://www.marsistemas.com",
+    "category": "Sales",
+    "depends": ["sale","base","sale_stock","fleet"],
+    "data":['taller_mecanico_view.xml'],
+    "demo_xml": [],
+    "update_xml": [],
+    "active": False,
+    "installable": True,
+    "certificate" : "",
+}
