fec67b6c5ed01b7214332d7610208f6886614320
Alan Ramos <alan196@gmail.com>
2014-02-28 08:02:48 -0700

    Cambios en vistas y dependencias

--- __openerp__.py
+++ __openerp__.py
@@ -12,7 +12,7 @@
     "author": "MAR Sistemas",
     "website": "http://www.marsistemas.com",
     "category": "Sales",
-    "depends": ["base","sale","sale_stock","fleet","sale_order_webkit"],
+    "depends": ["base","sale","sale_stock","fleet","sale_order_webkit","purchase"],
     "data":['taller_mecanico_view.xml','taller_vehicle_view.xml'],
     "demo_xml": [],
     "update_xml": [],
