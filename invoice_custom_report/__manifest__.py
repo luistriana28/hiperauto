79ae56ab9256f9aae90c8111f217b6dcdbf9ff5e
Luis M. Triana <luis.triana@jarsa.com.mx>
2018-02-27 12:41:57 -0600

    [FIX]custom_invoice_report: Make new fields for taxes in concepts (#3)

---
+++ invoice_custom_report/__manifest__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2017, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Invoice Report for Hiperauto",
+    "version": "10.0.0.1.0",
+    "category": "Report",
+    "author": "Jarsa Sistemas",
+    "depends": [
+        'report',
+        'account',
+        'sale',
+        'l10n_mx_base',
+    ],
+    "data": [
+        'report/l10n_mx_base_report.xml',
+    ],
+    "installable": True,
+}
