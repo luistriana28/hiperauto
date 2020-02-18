36b59805135437fa4f67e354088205977d934ab0
Your Name <you@example.com>
2020-02-18 15:20:33 -0600

    [FIX] init.py file & years for fleet vehicles

--- taller_mecanico/models/taller_vehicle.py
+++ taller_mecanico/models/taller_vehicle.py
@@ -6,7 +6,7 @@ from datetime import datetime
 class fleet_vehicle_year(models.Model):
     _inherit = 'fleet.vehicle'
 
-    years = [(num, str(num)) for num in range(1970, (datetime.now().year)+1)]
+    years = [(num, str(num)) for num in range(1970, (datetime.now().year)+2)]
 
     year = fields.Selection(years, 'Year')
     driver_id_custom = fields.Many2one(
