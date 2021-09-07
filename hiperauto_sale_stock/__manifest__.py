19c1d033ba172faa2e4be4d79686166ca948ce64
Luis Triana <luistriana.28@gmail.com>
2021-09-07 16:03:29 -0500

    [ADD]Welcome Sale Stock Restriction

---
+++ hiperauto_sale_stock/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021, LUIS TRIANA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'Hiperauto Sale Stock Restriction',
+    'summary': 'Module summary',
+    'version': '12.0.1.0.0',
+    'category': 'Uncategorized',
+    'website': 'https://odoo-community.org/',
+    'author': 'Luis Triana, Odoo Community Association (OCA)',
+    'license': 'LGPL-3',
+    'application': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
+    'depends': [
+        'sale_management'
+    ],
+    'data': [
+    ]
+}
---
+++ hiperauto_sale_stock/models/sale_order.py
@@ -0,0 +1,23 @@
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_confirm(self):
+        """
+        Super for Sale Order action confirm
+        """
+        for sale_order in self:
+            for sale_order_line in sale_order.order_line:
+                if sale_order_line.product_id.type in ['product']:
+                    product = sale_order_line.product_id
+                    available_qty = product.qty_available
+                    sale_qty = sale_order_line.product_uom_qty
+                    if not available_qty < sale_qty:
+                        continue
+                    raise ValidationError(_(
+                        'You cannot confirm order without stock. ' +
+                        'Product: ' + product.default_code + ' Stock: ' + str(available_qty)))
+        return super(SaleOrder, self).action_confirm()
